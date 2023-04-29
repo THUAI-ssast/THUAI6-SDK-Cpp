aff638902ac07f6ac694693fecf92e565ff184e3
j31234 <j31234@126.com>
2023-04-29 14:51:59 +0800

    delete consumer

--- sdk/main.cpp
+++ sdk/main.cpp
@@ -9,45 +9,17 @@ using json = nlohmann::json;
 
 using namespace std;
 
-class Consumer {
-public:
-    Consumer(int timeout = 1) : timeout(timeout) {
-        task = std::thread(&Consumer::run, this);
-    }
-
-    string get_init() {
-        while (lines.empty()) {};
-        return lines[0];
-    }
-
-    string get_observation() {
-        time_t begin = clock();
-        while (lines.size() == last_length)
-            if (clock() - begin > timeout * CLOCKS_PER_SEC)
-                exit(0);
-
-        last_length = lines.size();
-        return lines.back();
-    }
-
-private:
-    std::thread task;
-    void run() {
-        string line;
-        while (getline(cin, line)) lines.push_back(line);
-        cerr << "consumer thread exited" << endl;
-    }
-
-    vector<string> lines;
-    int last_length = 1;
-    int timeout;
-};
+string input() {
+    string line;
+    getline(cin, line);
+    return line;
+}
 
 int main() {
     FILE* logfile = fopen("ai.log", "w");
-    Consumer consumer;
+    string line;
 
-    string start_obs_str = consumer.get_init();
+    string start_obs_str = input();
     json start_obs = json::parse(start_obs_str);
 
     try {
@@ -60,7 +32,7 @@ int main() {
     }
 
     while (true) {
-        string obs_str = consumer.get_observation();
+        string obs_str = input();
         json obs = json::parse(obs_str);
         json action;
         try {
